f04fd7a735448197a8a6bfc1cf7f3bac55102cb3
Jan-Simon Moeller <jsmoeller@linuxfoundation.org>
2023-03-10 11:19:03 +0100

    More debug :D
    Signed-off-by: Jan-Simon Moeller <jsmoeller@linuxfoundation.org>

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 DATETAG=$(date +%Y-%m-%d)
 docker build --no-cache --network host --tag prhashserv:production-$DATETAG .
 docker run -p 8181:8181 -p 8282:8282 -p 8383:8383 -p 8484:8484  -d prhashserv:production-$DATETAG
